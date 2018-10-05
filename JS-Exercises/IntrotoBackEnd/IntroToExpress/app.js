15e6bcb31a24cc79681fd12ea2452d65c8bc0453
Khoi <khoin0412@gmail.com>
2018-10-04 22:32:39 -0500

    Finished exercise with express for server routes

--- JS-Exercises/IntrotoBackEnd/IntroToExpress/app.js
+++ JS-Exercises/IntrotoBackEnd/IntroToExpress/app.js
@@ -1,20 +1,38 @@
 let express = require('express');
 let app = express();
-// "/" => "Hi there!"
-app.get("/", function(req, res) {
-  res.send("Hi there!");
+
+app.get('/', function(req, res) {
+  res.send("Hi there, welcome to my assignment");
 });
 
-// "/bye" => "Goodbye!"
-app.get("/bye", function(req, res) {
-  res.send("Goodbye!");
-});
-// "/dog" => "MEOW!"
-app.get("/dog", function(req, res) {
-  res.send("MEOW!");
+app.get('/speak/pig', function(req, res) {
+  res.send("The pig says 'Oink'");
 });
 
-// Tell express to listen for request (start server)
+app.get('/speak/cow', function(req, res) {
+  res.send("The cow says 'Moo'");
+});
+
+app.get('/speak/dog', function(req, res) {
+  res.send("The dog says 'Woof Woof!'");
+});
+
+app.get('/repeat/:word/:number', function(req, res) {
+  let number = parseInt(req.params.number);
+  let word = req.params.word;
+  let str = '';
+  for(let i = 0; i < number; i++) {
+    str += word + ' ';
+  }
+  res.send(str);
+});
+
+
+app.get('*', function(req, res) {
+  res.send("Sorry, page not found...What are you doing with your life?");
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server has started!!!");
