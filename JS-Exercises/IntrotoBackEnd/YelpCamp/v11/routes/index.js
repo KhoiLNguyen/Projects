36e5ab0792b62088d7ae2d00811a5553d83408cf
Khoi <khoin0412@gmail.com>
2018-10-12 14:25:34 -0500

    Handle errors that could break the web app

---
+++ JS-Exercises/IntrotoBackEnd/YelpCamp/v11/routes/index.js
@@ -0,0 +1,50 @@
+let express = require("express");
+let router = express.Router();
+let passport = require("passport");
+let User = require("../models/user");
+
+// Landing page at root path
+router.get("/", function(req, res) {
+  res.render("landing");
+})
+
+// SHOW REGISTER FORM
+router.get("/register", function(req, res) {
+  res.render("register");
+});
+
+//handle signup logic
+router.post("/register", function(req, res) {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user) {
+    if(err) {
+      req.flash("error", err.message);
+      return res.redirect("register");
+    }
+    passport.authenticate("local")(req, res, function() {
+      req.flash("success", "Welcome to YelpCamp " + user.username);
+      res.redirect("/campgrounds");
+    })
+  });
+});
+
+// show login form
+router.get("/login", function(req, res) {
+  res.render("login");
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/campgrounds",
+  failureRedirect: "/login"
+}), function(req, res) {
+
+})
+
+// logout route
+router.get("/logout", function(req, res) {
+  req.logout();
+  req.flash("success", "Logged you out!");
+  res.redirect("/campgrounds");
+})
+
+module.exports = router;
